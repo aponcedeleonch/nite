7d00483a6884e55dad052f68b6fed7756eb16e83
jjj <notset@notset.com>
2025-02-03 22:12:50 +0200

    Fix linting

--- src/nite/audio/audio_file_to_stream.py
+++ src/nite/audio/audio_file_to_stream.py
@@ -1,30 +1,25 @@
+import io
+import wave
+
+import pyaudio
 from pydub import AudioSegment
 from pydub.playback import play
-import pyaudio
-import wave
-import io
 
 from nite.config import STREAM_CHUNK
 from nite.logging import configure_module_logging
 
 logger = configure_module_logging("nite.audio.audio_file_to_stream")
 
-DEFAULT_CHUNK = 1024
-
 
 class AudioFileToStream:
-    '''
+    """
     This class is responsible for converting an audio file to a stream.
     The input file is loaded into an AudioSegment and then converted into a stream.
-    '''
-    def __init__(
-            self,
-            file_name: str,
-            file_format: str
-        ) -> None:
+    """
+
+    def __init__(self, file_name: str, file_format: str) -> None:
         self.input_song = file_name
         self.format = file_format
-
 
     def play_as_stream(self):
         try:
@@ -49,22 +44,19 @@ class AudioFileToStream:
 
 
 class AudioWaveFileToStream:
-    '''
+    """
     This class is responsible for converting an audio file in wav format to a stream using PyAudio.
-    '''
-    def __init__(
-            self,
-            file_name: str,
-            chunk: int = STREAM_CHUNK
-            ) -> None:
+    """
+
+    def __init__(self, file_name: str, chunk: int = STREAM_CHUNK) -> None:
         self.chunk = chunk
-        self.input_file = wave.open(file_name, 'rb')
+        self.input_file = wave.open(file_name, "rb")
         self.pyaud = pyaudio.PyAudio()
         self.stream = self.pyaud.open(
             format=self.pyaud.get_format_from_width(self.input_file.getsampwidth()),
             channels=self.input_file.getnchannels(),
             rate=self.input_file.getframerate(),
-            output = True
+            output=True,
         )
         logger.info(f"Initializing with audio file: {file_name} with chunk size: {self.chunk}")
 
@@ -82,4 +74,3 @@ class AudioWaveFileToStream:
         finally:
             self.stream.close()
             self.pyaud.terminate()
-
